29662fbe21d29bfa1811bffbe89f9110b37d6ae0
Rollo <rollokb@gmail.com>
2019-01-31 19:00:59 +0000

    create images app

---
+++ zyper/images/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class ImageFile(models.Model):
+    created_at = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=255)
+    file_original = models.FileField()
+    file_thumb = models.FileField()
